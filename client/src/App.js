153afbac7effbb86646f236728e046be1dcb328c
rohit singh negi <rocknegi53@gmail.com>
2021-06-27 21:28:26 +0200

    complete UI for login and register

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./App.css";
+import { Login } from "./components/layout/auth/Login";
+import { Register } from "./components/layout/auth/Register";
 import { Landing } from "./components/layout/Landing";
 import { Navbar } from "./components/layout/Navbar";
 
@@ -9,6 +11,12 @@ const App = () => (
   <Router>
     <Navbar />
     <Route exact path="/" component={Landing} />
+    <section className="contianer">
+      <Switch>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
+    </section>
   </Router>
 );
 
