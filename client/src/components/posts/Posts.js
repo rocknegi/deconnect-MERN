59e11a7272b108f4cea33ebf285bea35734a86d6
rohit singh negi <rocknegi53@gmail.com>
2021-07-15 11:43:17 +0200

    Add posts fix on click to avatar redirect to profile

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import { getPosts } from "../../redux/actions/post";
 import Spinner from "../layout/Spinner";
 import PostItem from "./PostItem";
+import PostForm from "./PostForm";
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
@@ -21,6 +22,7 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
             <i className="fas fas-user"></i>
             Welcome to the community
           </p>
+          <PostForm />
           <div className="posts">
             {posts.map((post) => (
               <PostItem key={post._id} post={post} />
