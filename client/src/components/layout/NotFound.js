5a1391219c5f7b6174999f79cc0f0d92f4dd268f
rohit singh negi <rocknegi53@gmail.com>
2021-07-17 23:55:29 +0200

    add 404 page

---
+++ client/src/components/layout/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const NotFound = () => {
+  return (
+    <>
+      <h1 className="x-large text-primary">
+        <i className="fas fa-exclamation-traingle"></i> Page Not found
+      </h1>
+      <p className="large">Sorry,This page does not exist</p>
+    </>
+  );
+};
